e4b62c0d36923a26122f1007f3cf77424864dc59
ArKaRaShi <rnwz12345@gmail.com>
2024-07-15 22:36:43 +0700

    Add create new user when signin

---
+++ src/app/api/users/create/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+import { StatusCode } from "src/constants/statusCode";
+import { addNewUser } from "src/databases/firestore/userDoc";
+import { UserData } from "src/types/schema/user";
+import { withAuthAndVerify } from "src/utils/withAuth";
+
+export async function POST(request: NextRequest) {
+    try {
+        const tokenData = await withAuthAndVerify(request);
+        const { uid, name, profileImageUrl } = tokenData;
+        const userData: UserData = {
+            username: name,
+            profileImageUrl: profileImageUrl,
+            currentProjectIds: [],
+            historicalProjectIds: [],
+            supportProjects: [],
+            favoriteProjectIds: [],
+            agreement: false,
+        };
+        await addNewUser(uid, userData);
+        return NextResponse.json(
+            { message: "Create user successful" },
+            { status: StatusCode.CREATED }
+        );
+    } catch (error: any) {
+        return NextResponse.json(
+            { message: "Create user failed" },
+            { status: StatusCode.INTERNAL_SERVER_ERROR }
+        );
+    }
+}
