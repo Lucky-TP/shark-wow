4707d2d7a8e742ee5ac18ee4044ff62a5374c413
ArKaRaShi <rnwz12345@gmail.com>
2024-07-15 22:39:36 +0700

    Add admin firestore

--- src/libs/firebase/firebaseAdmin.ts
+++ src/libs/firebase/firebaseAdmin.ts
@@ -1,9 +1,11 @@
 import { initializeApp, applicationDefault, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
 
 const adminApp = getApps().length
     ? getApps()[0]
     : initializeApp({ credential: applicationDefault() });
 const auth = getAuth(adminApp);
+const db = getFirestore(adminApp);
 
-export { auth };
+export { auth, db };
--- src/libs/firebase/firebaseClient.ts
+++ src/libs/firebase/firebaseClient.ts
@@ -1,10 +1,12 @@
 import { initializeApp, getApps } from "firebase/app";
+
 import { getAuth } from "firebase/auth";
 import { firebaseConfig } from "./firebaseConfig";
 
 const clientApp = getApps().length
-	? getApps()[0]
-	: initializeApp(firebaseConfig, "clientApp");
+    ? getApps()[0]
+    : initializeApp(firebaseConfig, "clientApp");
+
 const auth = getAuth(clientApp);
 
 export { auth };
