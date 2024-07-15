85f895cce2c6c5ea99988c3ccf3989933221e811
ArKaRaShi <rnwz12345@gmail.com>
2024-07-15 22:39:20 +0700

    Add withAuthAndVerify

--- src/utils/withAuth.ts
+++ src/utils/withAuth.ts
@@ -1,11 +1,18 @@
 "use server";
 import { NextRequest } from "next/server";
 import { USER_TOKEN } from "src/constants/cookiesKeyName";
+import { verifyToken } from "./jwt";
+import { UserToken } from "src/interfaces/token";
 
 export async function withAuth(request: NextRequest) {
-    const userToken = request.cookies.get(USER_TOKEN);
+    const userToken = request.cookies.get(USER_TOKEN)?.value;
     if (!userToken) {
         throw new Error("Unauthorized");
     }
     return userToken;
+}
+
+export async function withAuthAndVerify(request: NextRequest) {
+    const userToken = await withAuth(request);
+    return verifyToken(userToken) as UserToken;
 }
