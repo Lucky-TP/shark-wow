b7ba74d55bc6d19831ffd9fda8a3821bc391d2df
ArKaRaShi <rnwz12345@gmail.com>
2024-07-21 16:27:32 +0700

    Add path, Change name

--- src/constants/statusCode.ts
+++ src/constants/statusCode.ts
@@ -3,7 +3,7 @@ export enum StatusCode {
     CREATED = 201,
     BAD_REQUEST = 400,
     UNAUTHORIZED = 401,
-    FORBIDDEN = 403,
+    ALREADY_EXISTS = 403,
     NOT_FOUND = 404,
     INTERNAL_SERVER_ERROR = 500,
 }
