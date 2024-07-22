928e2607d9fbcdd4a774c3c2d002343e794dcd6f
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-07-22 12:23:23 +0700

    Done homepage placholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Footer from "src/components/global/footer/containerFooter";
 import MainNav from "src/components/global/navigation/mainNav";
 import Herosection from "src/components/homepage/herosection";
+import PopularCreator from "src/components/homepage/popularCreator";
 import PopularProject from "src/components/homepage/popularProject";
 
 export default function RootPage() {
@@ -9,6 +10,7 @@ export default function RootPage() {
             <MainNav/>
             <Herosection/>
             <PopularProject/>
+            <PopularCreator/>
             <Footer/>
         </section>
     );
