11c7282555657d3e19dd7daf05de3a88493ca18d
Toey <thanalast99@gmail.com>
2024-07-25 23:26:46 +0700

    make sign in page

--- src/app/(global)/sign-in/page.tsx
+++ src/app/(global)/sign-in/page.tsx
@@ -1,10 +1,13 @@
 "use client";
+import SigninPlaceHolder from "src/components/authen/Signin";
 import { SignInWithGoogle } from "src/components/SignInWithGoogle";
+
 
 export default function SigninPage() {
     return (
         <>
-            <SignInWithGoogle />
+            <SigninPlaceHolder/>
+            {/* <SignInWithGoogle /> */}
         </>
     );
 }
