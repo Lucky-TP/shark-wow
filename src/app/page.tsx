33e79a7ccfab09f3b728f3fae4cb0ce31262ecc8
Lucky <luc.ky_tunchanok@hotmail.com>
2024-07-27 20:37:45 +0700

    Remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Footer from "src/components/global/footer/containerFooter";
-import MainNav from "src/components/global/navigation/mainNav";
 import Herosection from "src/components/homepage/herosection";
 import PopularCreator from "src/components/homepage/popularCreator";
 import PopularProject from "src/components/homepage/popularProject";
