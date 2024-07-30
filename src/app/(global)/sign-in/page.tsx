1dc0ea3d658b8bec2bdeb56b4e65a2ad9d7d5860
Toey <thanalast99@gmail.com>
2024-07-31 00:59:45 +0700

    Sign in update

--- src/app/(global)/sign-in/page.tsx
+++ src/app/(global)/sign-in/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 import SigninPlaceHolder from "src/components/auth/Signin";
-import SigninByToey from "src/components/authen/Signin";
 import SignInWithGoogle from "src/components/SignInWithGoogle";
 
 export default function SigninPage() {
     return (
         <>
-            {/* <SigninPlaceHolder/> */}
-            <SigninByToey/>
+            <SigninPlaceHolder/>
             {/* <SignInWithGoogle /> */}
         </>
     );
