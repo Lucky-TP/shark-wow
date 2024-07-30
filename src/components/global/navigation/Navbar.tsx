3fbbe6b1ea6dbb95f3bc0f9239e2f868a2c07da4
Lucky <luc.ky_tunchanok@hotmail.com>
2024-07-30 23:59:02 +0700

    Make sign-in, sign-out and user logo on Navbar

--- src/components/global/navigation/Navbar.tsx
+++ src/components/global/navigation/Navbar.tsx
@@ -1,42 +1,101 @@
+"use client"
 import Link from 'next/link';
 import React from 'react';
 
-import { MdAccountCircle } from "react-icons/md";
+import { useEffect, useState } from "react";
+import { useAuth } from "src/utils/useAuth";
+import { useRouter } from "next/navigation";
+import { UserModel } from "src/interfaces/models/user";
+import { apiPath, pagePath } from "src/constants/routePath";
+import axios, { AxiosResponse } from "axios";
+import { GetUserResponse } from "src/interfaces/response/userResponse";
+import { signOut } from 'src/services/authService';
+import { Dropdown } from 'antd';
 
 type Props = {}
 
 export default function Navbar({}: Props) {
-  return (
-    <section>
-        <nav className='px-[3vw] bg-gray-200 shadow-md py-[2vh]'>
-            <div className='flex flex-row justify-between items-center'>
-                <div className='flex items-center'>
-                    <Link href="/">
-                        <img src='/assets/shark.png' className='w-[40px] h-[40px] rounded-full' alt="SharkWow Logo" />
-                    {/* Shark Wow group */}
-                    </Link>
-                </div>
-                <ul className='flex flex-row gap-x-[3vw] items-center'>
-                    {/* <li>
-                        <Link href="/blogs" className='text-gray-800 hover:text-blue-500'>BLOGS</Link>
-                    </li> */}
-                    <li>
-                        <Link href="/create-project" className='text-gray-800 hover:text-blue-500'>CREATE PROJECT</Link>
-                    </li>
-                    <li>
-                        <Link href="/catagories" className='text-gray-800 hover:text-blue-500'>CATAGORIES</Link>
-                    </li>
-                    <li>
-                        <Link href="/sign-in" className='text-gray-800 hover:text-blue-500'>SIGN IN / SIGN UP</Link>
-                    </li>
-                    <li>
-                        <Link href="/my-account" className='text-gray-800 hover:text-blue-500 flex items-center'>
-                            <MdAccountCircle  className='text-3xl'/>
+    const [user, setUser] = useState<UserModel>();
+    const [loading, setLoading] = useState<boolean>(false);
+    const { user: authUser, authLoading } = useAuth();
+    const router = useRouter();
+    useEffect(() => {
+        const fetchUserProfile = async () => {
+            try {
+                setLoading(true);
+                const response: AxiosResponse<GetUserResponse> = await axios.get(apiPath.USERS.GET_SELF);
+                setUser(response.data.data);
+            } catch (error) {
+                console.error("Error fetching user profile:", error);
+            } finally {
+            setLoading(false);
+            }
+        };
+        if (!authLoading) {
+            fetchUserProfile();
+        }
+    }, [authLoading, authUser]);
+
+    const handleSignOut = async () => {
+        try {
+            await signOut();
+            router.push("/");
+        } catch (error: any) {
+            console.error("Error signing out:", error.message);
+        }
+    };
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <section>
+            <nav className='px-[3vw] bg-gray-200 shadow-md py-[2vh]'>
+                <div className='flex flex-row justify-between items-center'>
+                    <div className='flex items-center'>
+                        <Link href="/">
+                            <img src='/assets/shark.png' className='w-[40px] h-[40px] rounded-full' alt="SharkWow Logo" />
                         </Link>
-                    </li>
-                </ul>
-            </div>
-       </nav>
-    </section>
+                    </div>
+                    <ul className='flex flex-row gap-x-[3vw] items-center'>
+                        <li>
+                            <Link href="/create-project" className='text-gray-800 hover:text-blue-500'>CREATE PROJECT</Link>
+                        </li>
+                        <li>
+                            <Link href="/catagories" className='text-gray-800 hover:text-blue-500'>CATAGORIES</Link>
+                        </li>
+                        <li>
+                            {!user && (
+                                <Link href="/sign-in" className='text-gray-800 hover:text-blue-500'>SIGN IN / SIGN UP</Link>
+                            )}
+                            {user && (
+                            <Dropdown
+                                menu={{items: [
+                                    {
+                                        key: "1",
+                                        label: (
+                                            <div
+                                            onClick={handleSignOut}
+                                            >
+                                                sign out
+                                            </div>
+                                        ),
+                                        danger: true,
+                                    }
+                                ]}}
+                            >
+                                <img
+                                    src={user.profileImageUrl}
+                                    alt={`${user.firstName} ${user.lastName}`}
+                                    className="rounded-full w-12 h-12"
+                                />
+                            </Dropdown>
+                            )}
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </section>
   )
 }
