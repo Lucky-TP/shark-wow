fd3f1c175a0fd40db68d2e57f553ffdc12dd385f
lucky345 <luc.ky_tunchanok@hotmail.com>
2024-07-31 21:27:20 +0700

    Fix sign in page

--- src/components/auth/Signin.tsx
+++ src/components/auth/Signin.tsx
@@ -25,20 +25,14 @@ export default function SigninPlaceHolder({}: Props) {
   return (
     <section>
         <div className="h-screen w-screen flex bg-orange-300 ">
-            <div className="w-3/6 flex flex-col justify-center items-start p-16 ">
-                    <Link href="/">
-                    <img src="./assets/shark.png" alt="shark wow img" className="absolute top-10 left-14 w-14 h-14 rounded-full"/>
-                    </Link>
-                <div className='fixed top-20'>
+            <div className="w-3/6 flex flex-col items-start p-16 ">
+                <div>
                     <p className="text-white mb-5 mt-20">Welcome back</p>
                     <h1 className="text-white text-4xl font-medium">Sign in to Shark wow</h1>
                 </div>
             </div>
             <div className="w-full flex flex-col justify-center items-center bg-white p-16 rounded-tl-4xl">
                 <div className="w-full pl-20">
-                    <div className="text-right text-sm text-gray-600 mb-4 absolute top-10 right-14">
-                        <p>Don't have an account? <a href="#" className="underline text-orange-300 hover:text-orange-200">Sign up</a></p>
-                    </div>
                     <div className="flex flex-col mt-24">
                         <p className="font-medium text-gray-700 mb-4">Your account details</p>
                         <div className="w-full mb-4">
