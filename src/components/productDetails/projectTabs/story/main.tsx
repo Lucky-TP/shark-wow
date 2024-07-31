4cfcc2c02595223ebe163f5ec4ea4530583b3343
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-07-31 12:56:13 +0700

    story placeholder

--- src/components/productDetails/projectTabs/story/main.tsx
+++ src/components/productDetails/projectTabs/story/main.tsx
@@ -4,8 +4,11 @@ type Props = {}
 
 export default function MainProjectStory({}: Props) {
   return (
-    <section className='w-full bg-slate-200 h-[50vh]'>
-        story project
+    <section className='flex flex-row  w-full bg-slate-200 h-[50vh]'>
+        <section>
+            Getting querry by project details tags redering
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, vero. Excepturi error sapiente id totam aliquid enim illum neque! Excepturi corporis, debitis suscipit at totam in voluptatem aspernatur officiis numquam.
+        </section>
     </section>
   )
 }
