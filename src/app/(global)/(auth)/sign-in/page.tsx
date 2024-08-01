9015ed732fddb8aa3e9cdb0f238a02d546f1d29a
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-08-02 03:02:57 +0700

    Grouping authentication route structure

--- src/app/(global)/(auth)/sign-in/page.tsx
+++ src/app/(global)/(auth)/sign-in/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Signin from "src/components/auth/Signin";
-import SignInWithGoogle from "src/components/SignInWithGoogle";
 
 export default function SigninPage() {
     return (
