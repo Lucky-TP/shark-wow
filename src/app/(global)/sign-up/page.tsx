51c6eba7c98505f134359bef0f1a1086e6004fe7
Toey <thanalast99@gmail.com>
2024-08-01 19:10:54 +0700

    add signup page

---
+++ src/app/(global)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import SignupPlaceHolder from "src/components/auth/Signup";
+
+export default function SigninPage() {
+    return (
+        <>
+            <SignupPlaceHolder/>
+        </>
+    );
+}
