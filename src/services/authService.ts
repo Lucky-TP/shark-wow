889a95c30cb9760326284782f438c0fa815512a1
ArKaRaShi <rnwz12345@gmail.com>
2024-08-02 21:41:43 +0700

    Add sign-in and sign-up with email and password in authService to integrate with frontend pages

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -5,11 +5,18 @@ import {
     signOut as _signOut,
     onAuthStateChanged as _onAuthStateChanged,
     User,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
 } from "firebase/auth";
 import { auth } from "src/libs/firebase/firebaseClient";
 import { apiPath } from "src/constants/routePath";
-import { SignInPayload } from "src/interfaces/payload/userPayload";
+import {
+    EmailSignInPayload,
+    EmailSignUpWithToken,
+    UserIdTokenPayload,
+} from "src/interfaces/payload/authPayload";
 import { IS_COOKIE_SET } from "src/constants/sessionKeyName";
+import { EmailSignUpPayload } from "src/interfaces/payload/authPayload";
 
 export function onAuthStateChanged(callback: (user: User | null) => void) {
     return _onAuthStateChanged(auth, callback);
@@ -18,13 +25,47 @@ export function onAuthStateChanged(callback: (user: User | null) => void) {
 export async function signInWithGoogle() {
     try {
         const provider = new GoogleAuthProvider();
-        const result = await signInWithPopup(auth, provider);
-        const userIdToken = await result.user.getIdToken();
-        const payload: SignInPayload = { userIdToken };
-        await axios.post(apiPath.AUTH.GOOGLE_SIGNIN, payload);
+        const userCredential = await signInWithPopup(auth, provider);
+        const userIdToken = await userCredential.user.getIdToken();
+        const userIdTokenPayload: UserIdTokenPayload = { userIdToken };
+        await axios.post(apiPath.AUTH.GOOGLE_SIGNIN, userIdTokenPayload);
         sessionStorage.setItem(IS_COOKIE_SET, "true");
     } catch (error: any) {
-        console.log(error);
+        throw new Error("Sign-in with google failed");
+    }
+}
+
+export async function signInWithEmail(payload: EmailSignInPayload) {
+    try {
+        const { email, password } = payload;
+        const userCredential = await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+        const userIdToken = await userCredential.user.getIdToken();
+        const userIdTokenPayload: UserIdTokenPayload = { userIdToken };
+        await axios.post(apiPath.AUTH.EMAIL_SIGNIN, userIdTokenPayload);
+        sessionStorage.setItem(IS_COOKIE_SET, "true");
+    } catch (error: any) {
+        throw new Error("Sign-in with email failed");
+    }
+}
+
+export async function signUpWithEmail(payload: EmailSignUpPayload) {
+    try {
+        const { email, password } = payload;
+        const userCredential = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+        const userIdToken = await userCredential.user.getIdToken();
+        const newPayload: EmailSignUpWithToken = { ...payload, userIdToken };
+        await axios.post(apiPath.AUTH.EMAIL_SIGNUP, newPayload);
+        sessionStorage.setItem(IS_COOKIE_SET, "true");
+    } catch (error: any) {
+        throw new Error("Sign-up with email failed");
     }
 }
 
@@ -34,6 +75,6 @@ export async function signOut() {
         sessionStorage.removeItem(IS_COOKIE_SET);
         const result = await _signOut(auth);
     } catch (error: any) {
-        console.log(error);
+        throw new Error("Sign-out failed");
     }
 }
