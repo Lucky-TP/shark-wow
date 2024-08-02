b0c4e5dbc6a86007b66f69b5077844dd7ef70d3f
ArKaRaShi <rnwz12345@gmail.com>
2024-08-02 21:44:31 +0700

    Add function to translate string to second, Add function to translate milli-second to timestamp

--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -1,22 +1,52 @@
 import { Timestamp } from "firebase-admin/firestore";
 
-const GMT_7_MILLISEC = 7 * 60 * 60 * 1000;
+/**
+ * Translates a duration string (e.g., '1d', '1h', '30m', '45s') to seconds.
+ * @param duration - The duration string to translate.
+ * @returns The equivalent duration in seconds.
+ */
+export function translateDurationToSeconds(duration: string): number {
+    const timeUnits: { [key: string]: number } = {
+        d: 86400, // 1 day = 86400 seconds
+        h: 3600, // 1 hour = 3600 seconds
+        m: 60, // 1 minute = 60 seconds
+        s: 1, // 1 second = 1 second
+    };
 
-export function dateGmt0ToDateGmt7(date: Date): Date {
-    const gmt7Date = new Date(date.getTime() + GMT_7_MILLISEC);
-    return gmt7Date;
+    const regex = /(\d+)([dhms])/g;
+    let match;
+    let totalSeconds = 0;
+
+    while ((match = regex.exec(duration)) !== null) {
+        const value = parseInt(match[1], 10);
+        const unit = match[2];
+
+        if (timeUnits[unit] !== undefined) {
+            totalSeconds += value * timeUnits[unit];
+        }
+    }
+
+    return totalSeconds;
+}
+
+// export function dateGmt0ToDateGmt7(date: Date): Date {
+//     const gmt7MilliSecond = translateDurationToSeconds("7h") * 1000;
+//     const gmt7Date = new Date(date.getTime() + gmt7MilliSecond);
+//     return gmt7Date;
+// }
+
+export function milliToTimestamp(milliSecond: number): Timestamp {
+    return Timestamp.fromMillis(milliSecond);
 }
 
 export function dateToTimestamp(date: Date): Timestamp {
-    const timestamp = Timestamp.fromDate(date);
-    return timestamp;
+    return Timestamp.fromDate(date);
 }
 
-export function newTimestampGmt7(): Timestamp {
-    return dateToTimestamp(dateGmt0ToDateGmt7(new Date()));
-}
+// export function newTimestampGmt7(): Timestamp {
+//     return dateToTimestamp(dateGmt0ToDateGmt7(new Date()));
+// }
 
 export function timestampToDate(timestamp: Timestamp): Date {
-    const date = new Date(timestamp.toDate());
-    return date;
+    return new Date(timestamp.toDate());
 }
