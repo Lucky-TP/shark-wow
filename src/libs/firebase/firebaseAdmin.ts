e1de8e084965d612920ad831c1a29060a9ab23e0
ArKaRaShi <rnwz12345@gmail.com>
2024-08-02 10:48:50 +0700

    Integrate Firebase Admin from json service key to .env

--- src/libs/firebase/firebaseAdmin.ts
+++ src/libs/firebase/firebaseAdmin.ts
@@ -1,11 +1,28 @@
-import { initializeApp, applicationDefault, getApps } from "firebase-admin/app";
+import {
+    initializeApp,
+    applicationDefault,
+    getApps,
+    cert,
+} from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
-import { getStorage } from "firebase-admin/storage";
+
+// const adminApp = getApps().length
+//     ? getApps()[0]
+//     : initializeApp({ credential: applicationDefault() });
+
+const serviceAccount = JSON.parse(
+    Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT!, "base64").toString(
+        "ascii"
+    )
+);
 
 const adminApp = getApps().length
     ? getApps()[0]
-    : initializeApp({ credential: applicationDefault() });
+    : initializeApp({
+          credential: cert(serviceAccount),
+      });
+
 const auth = getAuth(adminApp);
 const db = getFirestore(adminApp);
 
