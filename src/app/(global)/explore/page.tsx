bf737080fae5691d7cedea69a5d04e9762f1c2f5
Toey <thanalast99@gmail.com>
2024-08-19 00:58:53 +0700

    just test add first of product

--- src/app/(global)/explore/page.tsx
+++ src/app/(global)/explore/page.tsx
@@ -6,8 +6,11 @@ type Props = {}
 export default function page({}: Props) {
   return (
     <section>
-      explore project page placeholder
-      <ProductCard/>
+      {/* explore project page placeholder
+      <ProductCard/> */}
+      <div className="p-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <ProductCard />
+    </div>
     </section>
   )
 }
--- src/components/productcard/ProductCard.tsx
+++ src/components/productcard/ProductCard.tsx
@@ -5,8 +5,26 @@ type Props = {}
 export default function ProductCard({}: Props) {
   return (
     <section>
-        <div className='bg-orange-200'>
-          <p>test</p>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+          <img 
+            className="w-full"
+            src="./assets/shark.png"
+            width={400}
+            height={250}
+          />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">title</div>
+            <p className="text-gray-700 text-base">
+              test
+            </p>
+          </div>
+          <div className="px-6 pt-4 pb-2">
+            <a 
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              View Project
+            </a>
+          </div>
         </div>
     </section>
   )
