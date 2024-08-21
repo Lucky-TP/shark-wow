ac42b08393596b402b7f43e0bcc44363539a9e6f
ArKaRaShi <rnwz12345@gmail.com>
2024-08-21 17:44:56 +0700

    Add key constants

---
+++ src/constants/payloadKeys/project.ts
@@ -0,0 +1,13 @@
+export const EditProjectPayloadKeys = {
+    name: "name",
+    description: "description",
+    carouselImageUrls: "carouselImageUrls",
+    status: "status",
+    address: "address",
+    categories: "categories",
+    stages: "stages",
+    story: "story",
+    payment: "payment",
+    website: "website",
+    update: "update",
+} as const;
