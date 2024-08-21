cfd47fb675c31bdaf38f5da9b2ee8afd94ac9314
Toey <thanalast99@gmail.com>
2024-08-22 00:10:38 +0700

    add scroll x with button and filter category

--- src/app/(global)/explore/page.tsx
+++ src/app/(global)/explore/page.tsx
@@ -1,29 +1,33 @@
-import React from 'react'
+import React from 'react';
 import MultiProductCard from 'src/components/productcard/multiproductcard/MultiProductCard';
 
-type Props = {}
+type Props = {};
 
-const categories = ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5'];
+const categories = ['Food', 'Technology', 'Art', 'File', 'House'];
 
 export default function page({}: Props) {
   return (
     <section className='flex flex-row justify-between py-[5vh] h-full w-full bg-orange-50'>
+      <div className="flex flex-col w-2/12">
+        <p className='text-xl font-bold text-left ml-8 mb-4'>Category</p>
+        {categories.map((category, i) => (
+          <button key={i} className="ml-8 mb-2 text-left">
+            {category}
+          </button>
+        ))}
+      </div>
 
-        <div className="flex flex-col w-2/12">
-          <p className='text-xl font-bold text-left ml-8 mb-4'>Category</p>
-          {categories.map((category, i) => (
-            <button key={i} className="ml-8 mb-2 text-left">
-              {category}  
-            </button>
-          ))}
-        </div>
+      <div className='text-xl flex flex-col w-10/12'>
+      <p className='pb-2'>Trending</p>
+        {/* First Row: All Products */}
+        <MultiProductCard />
 
-        <div className='text-xl flex flex-col w-10/12'>
-          <MultiProductCard/>
-          <MultiProductCard/>
-          <MultiProductCard/>
-        </div>
-        
+        {/* Second Row: Technology Products */}
+        <MultiProductCard category="Food" />
+
+        {/* Third Row: Food Products */}
+        <MultiProductCard category="Technology" />
+      </div>
     </section>
-  )
+  );
 }
--- src/mock/data/mockData.ts
+++ src/mock/data/mockData.ts
@@ -2,7 +2,7 @@ import { ShowProject } from "src/interfaces/models/common";
 export const productList: ShowProject[] = [
     {
         projectId: "1",
-        name: "hum yai",
+        name: "F hum yai",
         carouselImageUrls: ["./nuk.jpg"],
         description: "pom chob hee",
         stages: [{
@@ -14,7 +14,7 @@ export const productList: ShowProject[] = [
     },
     {
         projectId: "2",
-        name: "hum lek",
+        name: "F hum lek",
         carouselImageUrls: ["./assets/shark.png"],
         description: "pom gead hee",
         stages: [{
@@ -26,7 +26,7 @@ export const productList: ShowProject[] = [
     },
     {
         projectId: "3",
-        name: "hum noy",
+        name: "T hum noy",
         carouselImageUrls: ["./shark2.jpg"],
         description: "pom doo hee",
         stages: [{
@@ -38,7 +38,54 @@ export const productList: ShowProject[] = [
     },
     {
         projectId: "4",
-        name: "hum big",
+        name: "T hum big",
+        carouselImageUrls: ["./heart.png"],
+        description: "pom doo hee",
+        stages: [{
+            minimumFunding: 10,
+            currentFunding: 7000,
+            goalFunding: 10000
+        }],
+        category:"Technology"
+    },{
+        projectId: "5",
+        name: "F hum yai",
+        carouselImageUrls: ["./nuk.jpg"],
+        description: "pom chob hee",
+        stages: [{
+            minimumFunding: 20,
+            currentFunding: 5000,
+            goalFunding: 10000
+        }],
+        category:"Food"
+    },
+    {
+        projectId: "6",
+        name: "F hum lek",
+        carouselImageUrls: ["./assets/shark.png"],
+        description: "pom gead hee",
+        stages: [{
+            minimumFunding: 5,
+            currentFunding: 2000,
+            goalFunding: 10000
+        }],
+        category:"Food"
+    },
+    {
+        projectId: "7",
+        name: "T hum noy",
+        carouselImageUrls: ["./shark2.jpg"],
+        description: "pom doo hee",
+        stages: [{
+            minimumFunding: 15,
+            currentFunding: 9000,
+            goalFunding: 10000
+        }],
+        category:"Technology"
+    },
+    {
+        projectId: "8",
+        name: "T hum big",
         carouselImageUrls: ["./heart.png"],
         description: "pom doo hee",
         stages: [{
@@ -48,6 +95,102 @@ export const productList: ShowProject[] = [
         }],
         category:"Technology"
     },
+    {
+        projectId: "9",
+        name: "F hum yai",
+        carouselImageUrls: ["./nuk.jpg"],
+        description: "pom chob hee",
+        stages: [{
+            minimumFunding: 20,
+            currentFunding: 5000,
+            goalFunding: 10000
+        }],
+        category:"Food"
+    },
+    {
+        projectId: "10",
+        name: "F hum lek",
+        carouselImageUrls: ["./assets/shark.png"],
+        description: "pom gead hee",
+        stages: [{
+            minimumFunding: 5,
+            currentFunding: 2000,
+            goalFunding: 10000
+        }],
+        category:"Food"
+    },
+    {
+        projectId: "11",
+        name: "T hum noy",
+        carouselImageUrls: ["./shark2.jpg"],
+        description: "pom doo hee",
+        stages: [{
+            minimumFunding: 15,
+            currentFunding: 9000,
+            goalFunding: 10000
+        }],
+        category:"Technology"
+    },
+    {
+        projectId: "12",
+        name: "T hum big",
+        carouselImageUrls: ["./heart.png"],
+        description: "pom doo hee",
+        stages: [{
+            minimumFunding: 10,
+            currentFunding: 7000,
+            goalFunding: 10000
+        }],
+        category:"Technology"
+    },{
+        projectId: "13",
+        name: "F hum yai",
+        carouselImageUrls: ["./nuk.jpg"],
+        description: "pom chob hee",
+        stages: [{
+            minimumFunding: 20,
+            currentFunding: 5000,
+            goalFunding: 10000
+        }],
+        category:"Food"
+    },
+    {
+        projectId: "14",
+        name: "F hum lek",
+        carouselImageUrls: ["./assets/shark.png"],
+        description: "pom gead hee",
+        stages: [{
+            minimumFunding: 5,
+            currentFunding: 2000,
+            goalFunding: 10000
+        }],
+        category:"Food"
+    },
+    {
+        projectId: "15",
+        name: "T hum noy",
+        carouselImageUrls: ["./shark2.jpg"],
+        description: "pom doo hee",
+        stages: [{
+            minimumFunding: 15,
+            currentFunding: 9000,
+            goalFunding: 10000
+        }],
+        category:"Technology"
+    },
+    {
+        projectId: "16",
+        name: "T hum big",
+        carouselImageUrls: ["./heart.png"],
+        description: "pom doo hee",
+        stages: [{
+            minimumFunding: 10,
+            currentFunding: 7000,
+            goalFunding: 10000
+        }],
+        category:"Technology"
+    },
+    
 
     
     // Add more products as needed
