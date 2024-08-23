a483f4bfe671e5571f54445021b452ae79f33459
Toey <thanalast99@gmail.com>
2024-08-23 14:16:21 +0700

    add SearchBar to MainExplore

--- src/components/productcard/MainExplore.tsx
+++ src/components/productcard/MainExplore.tsx
@@ -3,7 +3,8 @@ import React, { useState } from 'react';
 import CarouselProductCard from 'src/components/productcard/CarouselProductCard';
 import CarouselTrendingProduct from 'src/components/productcard/CarouselTrendingProduct';
 import CategoryProductCard from 'src/components/productcard/CategoryProductCard';
-import Withmock from './withmock';
+import Withmock from './Withmock';
+import SearchBar from '../global/SearchBar';
 
 type Props = {};
 
@@ -16,54 +17,63 @@ export default function MainExplore({}: Props) {
   };
 
   return (
-    <section className='flex flex-row justify-between py-[5vh] h-full w-full bg-orange-50'>
-      <div className="flex flex-col w-2/12">
-      <p 
-          className='text-xl font-bold text-left ml-8 mb-4 cursor-pointer' 
-          onClick={() => handleCategoryClick(null)}
-        >
-          Category
-        </p>
-        {categories.map((category, i) => (
-          <button 
-            key={i} 
-            className={`ml-8 mb-2 text-left ${selectedCategory === category ? 'font-bold' : ''}`}
-            onClick={() => handleCategoryClick(category)}
-          >
-            {category}
-          </button>
-        ))}
+    <section className='flex flex-col justify-between py-[3vh] h-full w-full bg-orange-50'>
+      <div className='flex'>
+        <div style={{ width: `21%` }}>
+        </div>
+        <div className='pb-[3vh]' style={{ width: `73%` }}>
+          <SearchBar/>
+        </div>
       </div>
-        
-      <div className='text-xl flex flex-col w-10/12'>
-        {!selectedCategory && (
-          <>
-            {/* <Withmock category='Food'/> */}
-            <CarouselTrendingProduct />
-            <CarouselProductCard category="Food" />
-            <CarouselProductCard category="Technology" />
-            <CarouselProductCard category="Art" />
-            <CarouselProductCard category="Film" />
-            <CarouselProductCard category="House" />
-          </>
-        )}
+      <div className='flex'>
+        <div className="flex flex-col" style={{ width: `21%` }}>
+        <p 
+            className='text-xl font-bold text-left ml-12 mb-4 cursor-pointer' 
+            onClick={() => handleCategoryClick(null)}
+          >
+            Category
+          </p>
+          {categories.map((category, i) => (
+            <button 
+              key={i} 
+              className={`ml-12 mb-2 text-left ${selectedCategory === category ? 'font-bold' : ''}`}
+              onClick={() => handleCategoryClick(category)}
+            >
+              {category}
+            </button>
+          ))}
+        </div>
 
-        {selectedCategory === 'Food' && (
-          <CategoryProductCard category="Food" />
-        )}
-        {selectedCategory === 'Technology' && (
-          <CategoryProductCard category="Technology" />
-        )}
-        {selectedCategory === 'Art' && (
-          <CategoryProductCard category="Art" />
-        )}
-        {selectedCategory === 'Film' && (
-          <CategoryProductCard category="Food" />
-        )}
-        {selectedCategory === 'House' && (
-          <CategoryProductCard category="House" />
-        )}       
-        
+        <div className='text-xl flex flex-col' style={{ width: `73%` }}>
+          {!selectedCategory && (
+            <>
+              {/* <Withmock category='Food'/> */}
+              <CarouselTrendingProduct />
+              <CarouselProductCard category="Food" />
+              <CarouselProductCard category="Technology" />
+              <CarouselProductCard category="Art" />
+              <CarouselProductCard category="Film" />
+              <CarouselProductCard category="House" />
+            </>
+          )}
+
+          {selectedCategory === 'Food' && (
+            <CategoryProductCard category="Food" />
+          )}
+          {selectedCategory === 'Technology' && (
+            <CategoryProductCard category="Technology" />
+          )}
+          {selectedCategory === 'Art' && (
+            <CategoryProductCard category="Art" />
+          )}
+          {selectedCategory === 'Film' && (
+            <CategoryProductCard category="Food" />
+          )}
+          {selectedCategory === 'House' && (
+            <CategoryProductCard category="House" />
+          )}       
+          
+        </div>
       </div>
     </section>
   );
