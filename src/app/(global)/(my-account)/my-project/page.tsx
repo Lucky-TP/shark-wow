3432a7fdadcefcaeada52a07e5041813c77c6646
ToyGallagher <chanthawat555@gmail.com>
2024-08-23 14:58:05 +0700

    deletemock

--- src/app/(global)/(my-account)/my-project/page.tsx
+++ src/app/(global)/(my-account)/my-project/page.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Project from "src/components/account/project/project";
-import Mockproject from "src/components/account/project/mockproject";
 type Props = {}
 
 export default function page({}: Props) {
   return (
     <section className="">
             <Project/> 
-            <Mockproject/>
         </section>
   )
 }
