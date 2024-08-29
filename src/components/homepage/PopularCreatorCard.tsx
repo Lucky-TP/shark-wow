0a3cc22880762cc7e59ee9cb7b4cd4cf4b9c3135
Toey <thanalast99@gmail.com>
2024-08-30 01:31:32 +0700

    fix bug

--- src/components/homepage/PopularCreatorCard.tsx
+++ src/components/homepage/PopularCreatorCard.tsx
@@ -9,48 +9,53 @@ type Props = {}
 
 export default function PopularCreatorCard({}: Props) {
     const [data, setData] = useState<PopularCreator[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
+    const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<string | null>(null);
+    const fetchProducts = async () => {
+        try {
+            setLoading(true);
+            const data = await getTenPopularUsers();
 
+            setData(data.data);
+        } catch (error) {
+            setError("An error occurred while fetching products.");
+            console.error(
+                "An error occurred while fetching products:",
+                error
+            );
+        } finally {
+            setLoading(false);
+        }
+    };
     useEffect(() => {
-        const fetchProducts = async () => {
-            try {
-                const data = await getTenPopularUsers();
-
-                setData(data.data);
-            } catch (error) {
-                setError("An error occurred while fetching products.");
-                console.error(
-                    "An error occurred while fetching products:",
-                    error
-                );
-            } finally {
-                setLoading(false);
-            }
-        };
 
         fetchProducts();
     }, []);
 
   
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
+    return (
+        <>
+            {(loading) && <p>Loading...</p>}
+            {error && <p>Error: {error}</p>}
+            { (!loading) && data.length != 0 && 
+            <section className='bg-white p-10'>
+                <div className='flex flex-row items-center justify-between'>
+                    <h2 className="text-3xl font-bold mb-4">Popular Project</h2>
+                    <span>
+                        <Link href='/catargories'>
+                            <p>
+                                View All
+                            </p>
+                        </Link>
+                    </span>
+                </div>
+                <CarouselCreatorCard data={data}/>
+            </section>
+            }
+        </>
+    )
 
-  return (
-    <section className='bg-white p-10'>
-        <div className='flex flex-row items-center justify-between'>
-            <h2 className="text-3xl font-bold mb-4">Popular Creator</h2>
-            <span>
-                <Link href='/explore'>
-                    <p>
-                        View All
-                    </p>
-                </Link>
-            </span>
-        </div>
-        {/* mock ProductCard, TODO: implement CreatorCard */}
-        <CarouselCreatorCard data={data}/>
-    </section>
-  )
+
+  
 }
