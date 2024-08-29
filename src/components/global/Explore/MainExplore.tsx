89518f4aa52074d89eba43fa20038a82cd17d64c
Toey <thanalast99@gmail.com>
2024-08-29 18:04:11 +0700

    fetch data to component CarouselProductCard

--- src/components/global/Explore/MainExplore.tsx
+++ src/components/global/Explore/MainExplore.tsx
@@ -1,18 +1,67 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CarouselProductCard from 'src/components/ProductCard/CarouselProduct/CarouselProductCard';
-import CarouselTrendingProduct from 'src/components/ProductCard/CarouselProduct/CarouselTrendingProduct';
+import CarouselTrendingProductCard from './CarouselProductTopTen/CarouselTrendingProduct';
 import CategoryProductCard from './CategoryProduct/CategoryProductCard';
-import Withmock from '../../ProductCard/Withmock';
 import SearchBar from '../SearchBar';
+import { ShowProject } from 'src/interfaces/models/common';
+import { getProjectByCategories } from 'src/services/apiService/projects/getProjectByCategories';
 
-type Props = {};
+interface AllData {
+  categories: string[],
+  Food: ShowProject[],
+  Technology: ShowProject[],
+  Art: ShowProject[],
+  Film: ShowProject[],
+  House: ShowProject[],
+}
 
-export default function MainExplore({}: Props) {
+export default function MainExplore() {
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
   const categories = ['Food', 'Technology', 'Art', 'Film', 'House'];
+  const [data, setData] = useState<AllData>({
+    categories: ['Food', 'Technology', 'Art', 'Film', 'House'],
+    Food: [],
+    Technology: [],
+    Art: [],
+    Film: [],
+    House: []
+  });
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
-  const handleCategoryClick = (category: string| null) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const foodResponse = await getProjectByCategories(['Food']);
+        const technologyResponse = await getProjectByCategories(['Technology']);
+        const artResponse = await getProjectByCategories(['Art']);
+        const filmResponse = await getProjectByCategories(['Film']);
+        const houseResponse = await getProjectByCategories(['House']);
+
+        setData(prevData => ({
+          ...prevData,
+          Food: foodResponse.data,
+          Technology: technologyResponse.data,
+          Art: artResponse.data,
+          Film: filmResponse.data,
+          House: houseResponse.data,
+        }));
+      } catch (error) {
+        setError('An error occurred while fetching data.');
+        console.error('An error occurred while fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  const handleCategoryClick = (category: string | null) => {
     setSelectedCategory(category);
   };
 
@@ -21,13 +70,13 @@ export default function MainExplore({}: Props) {
       <div className='flex'>
         <div style={{ width: `21%` }}>
         </div>
-        <div className='pb-[3vh]' style={{ width: `73%` }}>
-          <SearchBar/>
+        <div className='pb-[3vh]' style={{ width: `73%` }}> 
+          <SearchBar />
         </div>
       </div>
       <div className='flex'>
         <div className="flex flex-col" style={{ width: `21%` }}>
-        <p 
+          <p 
             className='text-xl font-bold text-left ml-12 mb-4 cursor-pointer' 
             onClick={() => handleCategoryClick(null)}
           >
@@ -47,31 +96,17 @@ export default function MainExplore({}: Props) {
         <div className='text-xl flex flex-col' style={{ width: `73%` }}>
           {!selectedCategory && (
             <>
-              {/* <Withmock category='Food'/> */}
-              <CarouselTrendingProduct />
-              <CarouselProductCard category="Food" />
-              <CarouselProductCard category="Technology" />
-              <CarouselProductCard category="Art" />
-              <CarouselProductCard category="Film" />
-              <CarouselProductCard category="House" />
+              <CarouselTrendingProductCard />
+              <CarouselProductCard title="Food" data={data.Food} />
+              <CarouselProductCard title="Technology" data={data.Technology} />
+              <CarouselProductCard title="Art" data={data.Art} />
+              <CarouselProductCard title="Film" data={data.Film} />
+              <CarouselProductCard title="House" data={data.House} />
             </>
           )}
-
-          {selectedCategory === 'Food' && (
-            <CategoryProductCard category="Food" />
+          {selectedCategory && (
+            <CategoryProductCard category={selectedCategory} />
           )}
-          {selectedCategory === 'Technology' && (
-            <CategoryProductCard category="Technology" />
-          )}
-          {selectedCategory === 'Art' && (
-            <CategoryProductCard category="Art" />
-          )}
-          {selectedCategory === 'Film' && (
-            <CategoryProductCard category="Food" />
-          )}
-          {selectedCategory === 'House' && (
-            <CategoryProductCard category="House" />
-          )}       
           
         </div>
       </div>
