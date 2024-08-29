d6dc67e2056638b946e6ba17a847438b4a91d6e3
Toey <thanalast99@gmail.com>
2024-08-29 18:03:24 +0700

    create single and carousel creator card

---
+++ src/components/CreatorCard/CarouselCreator/CarouselCreatorCard.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import React, { useRef } from "react";
+import SingleCreatorCard from "../SingleCreator/SingleCreatorCard";
+import { PopularCreator } from "src/interfaces/models/common"; // Adjust the import path as needed
+
+type Props = {
+    data: PopularCreator[]; // Array of products passed as a prop
+};
+
+export default function CarouselCreatorCard({ data }: Props) {
+    const carouselRef = useRef<HTMLDivElement>(null);
+    
+    const scrollLeft = () => {
+        if (carouselRef.current) {
+            carouselRef.current.scrollBy({
+                left: -carouselRef.current.clientWidth / 4,
+                behavior: "smooth",
+            });
+        }
+    };
+
+    const scrollRight = () => {
+        if (carouselRef.current) {
+            carouselRef.current.scrollBy({
+                left: carouselRef.current.clientWidth / 4,
+                behavior: "smooth",
+            });
+        }
+    };
+
+    return (
+        <section className="relative">
+            <div className="relative">
+                <button
+                    onClick={scrollLeft}
+                    className="absolute -left-4 top-1/2 transform -translate-y-1/2 z-10 bg-transparent hover:bg-orange-400 p-2 rounded-full flex items-center justify-center h-12 w-12 border border-gray-300 hover:border-gray-500 transition-all duration-300"
+                    aria-label="Scroll Left"
+                >
+                    <span className="sr-only">Scroll Left</span>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6 text-gray-500 hover:text-gray-700"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M15 19l-7-7 7-7"
+                        />
+                    </svg>
+                </button>
+                <div className="pt-2 pb-4 overflow-x-hidden" ref={carouselRef}>
+                    <ul className="flex space-x-8">
+                        {data.map((creator) => (
+                            <li
+                                key={creator.firstName}
+                                className="flex-shrink-0"
+                                style={{ width: "calc(25% - 32px)" }}
+                            >
+                                <SingleCreatorCard creator={creator} />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <button
+                    onClick={scrollRight}
+                    className="absolute -right-5 top-1/2 transform -translate-y-1/2 z-10 bg-transparent hover:bg-orange-400 p-2 rounded-full flex items-center justify-center h-12 w-12 border border-gray-300 hover:border-gray-500 transition-all duration-300"
+                    aria-label="Scroll Right"
+                >
+                    <span className="sr-only">Scroll Right</span>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6 text-gray-500 hover:text-gray-700"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M9 5l7 7-7 7"
+                        />
+                    </svg>
+                </button>
+            </div>
+        </section>
+    );
+}
---
+++ src/components/CreatorCard/SingleCreator/SingleCreatorCard.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { PopularCreator } from 'src/interfaces/models/common';
+
+interface CreatorCardProps {
+    creator: PopularCreator;
+}
+
+const SingleCreatorCard = ({ creator }: CreatorCardProps) => {
+    
+    return (
+        <section>
+            <div className='pl-6 p-3'>
+                <div className="w-full h-full rounded-lg overflow-hidden relative group">
+                    <div className="relative w-full h-48"> {/* Fixed height for the image container */}
+                        <img
+                            className="w-full h-full object-contain" // Ensure the image fits within the container without cropping
+                            src={creator.profileImageUrl}
+                            alt={creator.firstName}
+                            width={400}
+                            height={400}
+                        />
+                        {/* Hover elements */}
+                        <div className="absolute inset-0 bg-black bg-opacity-40 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity duration-300">
+                            <button className="absolute bottom-2 bg-orange-600 text-white font-semibold py-2 px-4 rounded-full">
+                                View Creator
+                            </button>
+                        </div>
+                        <div className="absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            <button className="bg-orange-600 text-white p-2 rounded-full">
+                                <svg className="w-5 h-5" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <path d="M19 21l-7-5.3L5 21V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                    <div className="pt-3 py-1">
+                        <h2 className="text-xl font-semibold text-gray-800">{creator.firstName}<span> </span>{creator.lastName}</h2>
+                    </div>
+                    
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default SingleCreatorCard;
