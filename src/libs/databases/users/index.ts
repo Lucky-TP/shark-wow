2e591f2c0ff6e47bce8ab29ded95882729e664ba
ArKaRaShi <rnwz12345@gmail.com>
2024-08-30 15:13:34 +0700

    Add fallback deleting user in firestore when sign-in/sign-up failed

--- src/libs/databases/users/index.ts
+++ src/libs/databases/users/index.ts
@@ -2,3 +2,4 @@ export { addNewUser } from "./addNewUser";
 export { createUser } from "./createUser";
 export { getUser } from "./getUser";
 export { updateUser } from "./updateUser";
+export { deleteUser } from "./deleteUser";
