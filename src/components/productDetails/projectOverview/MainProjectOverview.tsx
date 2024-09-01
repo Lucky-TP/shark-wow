bc988f2a758116aeedc7dfb81b4f7496947d6c56
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-09-02 02:41:22 +0700

    Dataflow config Project overview

--- src/components/productDetails/projectOverview/MainProjectOverview.tsx
+++ src/components/productDetails/projectOverview/MainProjectOverview.tsx
@@ -12,19 +12,18 @@ export default function MainProjectOverview(
     { 
         projectData,
         isLoading,
-        error
+      
     }: ProjectDetialResponse
     ){
-    console.log('projectData',projectData?.carouselImageUrls)
+
     return (
       <section className='flex flex-row justify-between py-[5vh] h-full w-full bg-orange-50'>
-
               <>
                 <div className='w-8/12'>
                   <ProjectCarousel isLoading={isLoading} images={projectData?.carouselImageUrls}/>        
                 </div>
                 <div className='w-4/12'>
-                  <ProjectOverview/>
+                  <ProjectOverview  isLoading={isLoading} projectData={projectData} />
                 </div>          
               </>              
           
