86fd40b18e3f1ab3c4d078a8e75d7d228ea9b6d8
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-09-02 18:10:30 +0700

    Styling stage width in project tabs

--- src/components/productDetails/projectTabs/stage/TargetStage.tsx
+++ src/components/productDetails/projectTabs/stage/TargetStage.tsx
@@ -13,9 +13,9 @@ export default function TargetStage({ stage }: Props) {
     return (
         <li
             key={stage.stageId}
-            className="flex flex-col items-center justify-between bg-orange-100 md:min-w-[40vw] max-w-[40vw] h-[60vh] px-[2vw] py-[3vh]
+            className="flex flex-col items-center justify-between bg-orange-100 md:min-w-[50vw] max-w-[50vw] h-[60vh] px-[2vw] py-[3vh]
         rounded-lg  hover:shadow-xl hover:scale-[1.02] hover:bg-orange-200 border border-orange-200 shadow-xl
-        transition-all duration-700 hover:translate-y-[-2vh] cursor-pointer"
+        transition-all duration-700 hover:translate-y-[-1vh] cursor-pointer"
         >
             {
                 stage.imageUrl !== "" && 
