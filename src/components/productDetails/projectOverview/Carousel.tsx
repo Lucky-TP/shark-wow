caad3530a82879a0d13d7d4f497b577f9fa3b178
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-09-02 13:23:22 +0700

    Refactor useProjectDetails to Carousel project overview

--- src/components/productDetails/projectOverview/Carousel.tsx
+++ src/components/productDetails/projectOverview/Carousel.tsx
@@ -1,6 +1,9 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 
+import { useProjectDetails } from 'src/context/custom-hooks/useProjectDetails'
+
+
 import EmblaCarousel from './emblaCarousel/EmblaCarousel'
 import { EmblaOptionsType } from 'embla-carousel'
 
@@ -12,23 +15,22 @@ type Props = {
 
 const OPTIONS: EmblaOptionsType = { dragFree: true, loop: true }
 
-
-
-export default function ProjectCarousel({images , isLoading}: Props) {
+export default function ProjectCarousel() {
+    const context = useProjectDetails();
     const [slides, setSlides] = useState<any[]>([])
 
     useEffect(() => {
-        const temp : any[] = images?.map((e,i)=>{
+        const images : any[] = context.data.carouselImageUrls?.map((e,i)=>{
             return {
                 id: i+1,
                 image: e
             }
         }) || []
-        setSlides(temp)
-    }, [images])
+        setSlides(images)
+    }, [context])
     return (
         <section className='flex w-full h-full sm:px-[3vw] md:px-[2vw]'>
-            <EmblaCarousel slides={slides} options={OPTIONS} isLoading={isLoading} />
+            <EmblaCarousel slides={slides} options={OPTIONS} />
         </section>
     )
 }
