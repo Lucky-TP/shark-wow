7afe68cac7e1bc67bbe8cc0810e74c36b77136dc
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-09-02 18:10:55 +0700

    Refactor project update tabs to useProjectDetails

--- src/components/productDetails/projectTabs/updates/MainProjectUpdates.tsx
+++ src/components/productDetails/projectTabs/updates/MainProjectUpdates.tsx
@@ -1,100 +1,77 @@
 import React, { useEffect, useState } from 'react'
 
+import { useProjectDetails } from 'src/context/custom-hooks/useProjectDetails'
+
 import UpdateBlog from './UpdateBlog'
 
 import { Update } from 'src/interfaces/models/project'
 import { Divider } from 'antd'
 
-interface updatesStateType { 
-    update : Update[],
-    loading :  boolean,
-    currentUpdate : number ,
-}
-
-const exampleData : Update[] = [
-    {
-      id: 1,
-      detail: "Creating project number 1",
-      date: new Date(),
-      belongTo: 0,
-    },
-    {
-      id: 2,
-      detail: "Hey guys we are updating",
-      date: new Date(),
-      belongTo: 3,
-    },
-    {
-      id: 3,
-      detail: "Prototype stage is designing now ",
-      date: new Date(),
-      belongTo: 1,
-    },
-    {
-      id: 4,
-      detail: "Prototype done",
-      date: new Date(),
-      belongTo: 1,
-    },
-    {
-      id: 5,
-      detail: "Production we found it brother",
-      date: new Date(),
-      belongTo: 2,
-    }
-]
-
 export default function MainProjectUpdates() {
+    const {
+      ProjectInfo,
+      isLoading,
+    } = useProjectDetails()
 
-  const [updateState, setUpdateState] = useState<updatesStateType>({
-    update: [],
-    loading: true,
-    currentUpdate: 0,
-  })
-
-  useEffect(()=>{
-
-    setUpdateState({
-      ...updateState,
-      update: exampleData,
-      loading: false,
-    })
-      
-  },[])
-
-  return (
-      <section className='flex flex-row w-full justify-between px-[5vw]'>
-          <nav className='flex flex-col  min-w-fit items-center'>
-              <h1 className='text-2xl font-semibold  mb-[3vh]'>Progression</h1>
-              <ul className='flex flex-col gap-y-[3vh] '>
-                  {updateState.update.map((update,index) => (
+    const [currentUpdate, setUpdateState] = useState(0)
+    return (
+        <section className='flex flex-row w-full gap-x-[2vw] px-[5vw]'>
+            <nav className='flex flex-col  min-w-fit items-center'>
+                <h1 className='text-2xl font-semibold  mb-[3vh]'>Progression</h1>
+                <ul className='flex flex-col gap-y-[3vh] max-w-[27vw]'>
+                    {ProjectInfo.update && ProjectInfo.update.map((update,index) => (
+                        <li
+                          key={update.id}
+                          onClick={()=>{
+                            setUpdateState(index)                           
+                          }}
+                          className={`flex items-center justify-center p-3 bg-orange-200 rounded-xl w-full px-[1vw] shadow-lg
+                                    hover:bg-orange-300 hover:scale-[1.02] duration-500 transition-all cursor-pointer ${currentUpdate === index ? 'bg-orange-300' : 'bg-orange-200'}`}
+                        >
+                            <p className='text-lg text-gray-700 font-medium text-center'>
+                              {update.detail}
+                            </p>
+                        </li>
+                    ))}
+                    {
+                      ProjectInfo.update && ProjectInfo.update.length === 0 &&
                       <li
-                        key={update.id}
-                        onClick={()=>{
-                          setUpdateState({
-                            ...updateState,
-                            currentUpdate: index
-                          })
-                        }}
                         className='flex items-center justify-center p-3 bg-orange-300 rounded-xl w-full px-[1vw] shadow-lg
-                                  hover:bg-orange-400 hover:scale-[1.02] duration-500 transition-all cursor-pointer '
+                                  hover:bg-orange-300 hover:scale-[1.02] duration-500 transition-all cursor-pointer '
                       >
                           <p className='text-lg text-gray-700 font-medium text-center'>
-                            {update.detail}
+                            There're no currently updated yet.
                           </p>
                       </li>
-                  ))}
-              </ul>
-          </nav>
-          <Divider
-              type="vertical"
-              className='h-full flex mx-[2vw] bg-gray-700'
-          />
-          <div className=''>
-            { !updateState.loading && <UpdateBlog
-                data={updateState.update[updateState.currentUpdate]}
-            />}
-          </div>
-      </section>
-  )
+                    }
+                </ul>
+            </nav>
+            <Divider
+                type="vertical"
+                className='h-full flex mx-[2vw] bg-gray-700'
+            />
+            <div className='w-full'>
+              { !isLoading && ProjectInfo.update && ProjectInfo.update.length > 0  &&  
+                <UpdateBlog
+                  data={ProjectInfo?.update[currentUpdate]}
+                />
+              }
+              {
+                !isLoading && ProjectInfo.update?.length === 0 && 
+                   <section className='w-full'>
+                    <div
+                        className="flex flex-col items-center justify-center text-center bg-orange-100 w-full min-h-[60vh] px-[2vw] py-[3vh]
+                        rounded-lg  hover:shadow-xl hover:scale-[1.02] hover:bg-orange-200 border border-orange-200 shadow-xl
+                        transition-all duration-700 hover:translate-y-[-1vh] cursor-pointer"
+                    >
+                      <p className='text-3xl font-medium'>
+                        Waiting for creator update there progress. 
+                      </p>
+                    </div>
+
+                  </section>
+              }
+            </div>
+        </section>
+    )
 }
--- src/components/productDetails/projectTabs/updates/UpdateBlog.tsx
+++ src/components/productDetails/projectTabs/updates/UpdateBlog.tsx
@@ -8,10 +8,10 @@ type Props = {
 }
 
 export default function UpdateBlog({ data }: Props) {
-  console.log(data)
+  // console.log(data)
   return (
     <section className='h-[150vh]'>
-      <div className='flex flex-col px-[3vw] py-[3vh] rounded-xl border bg-orange-100 border-gray-700 h-full'>
+      <div className='flex flex-col px-[3vw] py-[3vh] rounded-xl border bg-orange-100 border-orange-200 h-full'>
         <h1>Update Topic</h1>
         <div className='flex flex-row gap-x-[1vw] '>
           <p>Posted on: {data.date.toDateString()}</p>
@@ -21,7 +21,6 @@ export default function UpdateBlog({ data }: Props) {
           Long text Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa earum fugiat ipsum beatae, quis, voluptates voluptatum, dignissimos aliquid eligendi odit illum quam tempora expedita aut facilis quae sequi doloremque nam.
         </div>        
       </div>
-
     </section>
   )
 }
