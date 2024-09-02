8e7cd2fda289c7c8c675416fc63220e7dcee0da6
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-09-02 16:14:20 +0700

    Refactor useProjectDetails

--- src/components/productDetails/projectOverview/ProjectOverview.tsx
+++ src/components/productDetails/projectOverview/ProjectOverview.tsx
@@ -4,32 +4,12 @@ import ProjectOverviewInfo from './overview/ProjectOverviewInfo'
 import ProjectStat from './overview/ProjectStat'
 import InteractProject from './overview/InteractProject'
 
-import { ProjectDetialResponse } from 'src/interfaces/datas/projectDetailStatus'
-
-import { Stage } from 'src/interfaces/models/project'
-
-export default function ProjectOverview({
-    isLoading,
-    projectData
-  }: Partial<ProjectDetialResponse>
-){
+export default function ProjectOverview(){
   return (
     <section className='flex flex-col h-full w-full pl-[2vw] pr-[4vw] gap-y-[1vh] '>
-        <ProjectOverviewInfo
-          isLoading={isLoading}
-          uid={projectData?.uid}
-          stageId={projectData?.status}
-          name={projectData?.name}
-          description={projectData?.description}
-        />
-        <ProjectStat
-          isLoading={isLoading}
-          // currentFunding={projectData?.currentFunding as number}  
-          currentFunding={0}
-          totalSupporter={projectData?.totalSupporter as number}
-          stages={projectData?.stages as Stage[]}
-        />
-        <InteractProject/> 
+        <ProjectOverviewInfo/>
+        <ProjectStat/>
+        <InteractProject /> 
     </section>
   )
 }
