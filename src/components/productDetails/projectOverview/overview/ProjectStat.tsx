2584e6f0745cd76792d35a827b3380ee46679b0a
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-09-02 16:05:14 +0700

    Refactor useProjectDetails to project stat wait for services current stage

--- src/components/productDetails/projectOverview/overview/ProjectStat.tsx
+++ src/components/productDetails/projectOverview/overview/ProjectStat.tsx
@@ -1,21 +1,48 @@
 import React from 'react'
 
-type Props = {}
+import { Stage } from 'src/interfaces/models/project'
 
-export default function ProjectStat({}: Props) {
-  return (
-    <>
-        <div className="flex justify-between items-center mb-2">
-            <span className="text-2xl font-bold">$ 5000 USD</span>
-            <span className="text-gray-600">10 backers</span>
-        </div>
-        <div className="w-full bg-gray-300 h-1 rounded-full mb-2">
-            <div className="bg-orange-500 h-full rounded-full" style={{ width: '50%' }}></div>
-        </div>
-        <div className="flex justify-between items-center text-gray-600">
-            <span>50% of $ 10000</span>
-            <span>43 days left</span>
-        </div>
-    </>
-  )
+import { Skeleton } from 'antd'
+import { useProjectDetails } from 'src/context/custom-hooks/useProjectDetails'
+
+export default function ProjectStat() {
+    const {
+      ProjectInfo,
+      isLoading,
+      error,
+    } = useProjectDetails(); 
+    return (
+      <>
+          {
+            isLoading &&
+            <Skeleton active/>
+          }
+          {
+            !isLoading &&
+            <>
+              <div className="flex justify-between items-center mb-2">
+                {/* implement project info current stage later */}
+                  <span className="text-2xl font-bold">$ {ProjectInfo.status} USD</span>
+                  <span className="text-gray-600">{ProjectInfo.totalSupporter} backers</span>
+              </div>
+              <div className="w-full bg-gray-300 h-1 rounded-full mb-2">
+                <div
+                    className="bg-orange-400 h-full rounded-full"
+                    style={{
+                        width: `${
+                          50
+                            // ((stages[currentStage !== undefined ? currentStage : 0].currentFunding + 1) / stages[currentStage !== undefined ? currentStage : 0].goalFunding)
+                        }%`
+                    }}
+                  />
+              </div>
+              <div className="flex justify-between items-center text-gray-600">
+                  {/* <span>{stages[currentStage].currentFunding+1} of $ 10000</span> */}
+                  <span>69 days left</span>
+              </div>        
+            </>
+          }
+
+      </>
+    )
 }
