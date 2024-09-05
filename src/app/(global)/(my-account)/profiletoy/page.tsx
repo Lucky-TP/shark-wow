ce83c60aa0877efa81f894963589a2561465f40e
lucky345 <luc.ky_tunchanok@hotmail.com>
2024-09-06 01:18:30 +0700

    Fix profile page

--- src/app/(global)/(my-account)/profiletoy/page.tsx
+++
@@ -1,9 +0,0 @@
-import UserProfile from "src/components/account/profile/UserProfile"; // Profile ที่นำเข้ามาจากที่อื่น
-
-export default function page() {
-    return (
-        <section className="">
-            <UserProfile/>  {/* ใช้ Profile ที่นำเข้ามา */}
-        </section>
-    );
-}
