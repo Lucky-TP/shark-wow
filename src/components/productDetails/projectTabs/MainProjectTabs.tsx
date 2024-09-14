b6985d11f923f1782e39c804088d6bd5f17ade99
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-09-14 12:47:54 +0700

    Refactor color for each tabs

--- src/components/productDetails/projectTabs/MainProjectTabs.tsx
+++ src/components/productDetails/projectTabs/MainProjectTabs.tsx
@@ -14,32 +14,32 @@ export default function MainProjectTabs() {
         <section>
             <ul className="flex flex-row justify-between shadow-xl ">
                 <li
-                    className={` ${activeTab === "story" ? "bg-orange-300" : "bg-orange-200"}
-                    w-full h-full flex items-center text-center justify-center py-[3vh] hover:bg-orange-400 cursor-pointer duration-500 transition-colors`}
+                    className={` ${activeTab === "story" ? "bg-orange-200" : "bg-orange-100"}
+                    w-full h-full flex items-center text-center justify-center py-[3vh] hover:bg-orange-200 cursor-pointer duration-500 transition-colors`}
                     onClick={() => setActiveTab("story")}
                 >
-                    <h2 className="text-xl text-gray-700 font-semibold">Story</h2>
+                    <h2 className="text-xl text-gray-600 font-semibold">Story</h2>
                 </li>
                 <li
-                    className={` ${activeTab === "update" ? "bg-orange-300" : "bg-orange-200"}
-                    w-full h-full flex items-center text-center justify-center py-[3vh] hover:bg-orange-400 cursor-pointer duration-500 transition-colors`}
+                    className={` ${activeTab === "update" ? "bg-orange-200" : "bg-orange-100"}
+                    w-full h-full flex items-center text-center justify-center py-[3vh] hover:bg-orange-200 cursor-pointer duration-500 transition-colors`}
                     onClick={() => setActiveTab("update")}
                 >
-                    <h2 className="text-xl text-gray-700 font-semibold">Updates</h2>
+                    <h2 className="text-xl text-gray-600 font-semibold">Updates</h2>
                 </li>
                 <li
-                    className={` ${activeTab === "discussion" ? "bg-orange-300" : "bg-orange-200"}
-                    w-full h-full flex items-center text-center justify-center py-[3vh] hover:bg-orange-400 cursor-pointer duration-500 transition-colors`}
+                    className={` ${activeTab === "discussion" ? "bg-orange-200" : "bg-orange-100"}
+                    w-full h-full flex items-center text-center justify-center py-[3vh] hover:bg-orange-200 cursor-pointer duration-500 transition-colors`}
                     onClick={() => setActiveTab("discussion")}
                 >
-                    <h2 className="text-xl text-gray-700 font-semibold">Discussion</h2>
+                    <h2 className="text-xl text-gray-600 font-semibold">Discussion</h2>
                 </li>
                 <li
-                    className={` ${activeTab === "to-creator" ? "bg-orange-300" : "bg-orange-200"}
-                    w-full h-full flex items-center text-center justify-center py-[3vh] hover:bg-orange-400 cursor-pointer duration-500 transition-colors`}
+                    className={` ${activeTab === "to-creator" ? "bg-orange-200" : "bg-orange-100"}
+                    w-full h-full flex items-center text-center justify-center py-[3vh] hover:bg-orange-200 cursor-pointer duration-500 transition-colors`}
                     onClick={() => setActiveTab("to-creator")}
                 >
-                    <h2 className="text-xl text-gray-700 font-bold">To Creator</h2>
+                    <h2 className="text-xl text-gray-600 font-bold">To Creator</h2>
                 </li>
             </ul>
             <section className="py-[3vh] bg-orange-50 min-h-[160vh] h-fit overflow-y-auto px-[3vw]">
