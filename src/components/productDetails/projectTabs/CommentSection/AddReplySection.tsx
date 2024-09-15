8b2d85e6958e0e5cc1a5df59c31b20bd82fc5740
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-09-16 00:09:49 +0700

    refactor ui in adding reply section remove profiles details

--- src/components/productDetails/projectTabs/CommentSection/AddReplySection.tsx
+++ src/components/productDetails/projectTabs/CommentSection/AddReplySection.tsx
@@ -72,47 +72,11 @@ export default function AddReplySection({ currentUser , parentComment }: Props) 
             <div className='flex flex-row w-full'>
                 <div className='w-[1vw] min-h-fit bg-orange-300 rounded-l-lg block'/>  
                 <div className='flex flex-col  bg-orange-100 p-4 border border-orange-300 rounded-r-xl w-full gap-y-[2vh]'>
-                    <div className='flex flex-row gap-x-[2vw] w-full items-center'>
-                        {
-                            currentUser?.username && 
-                            <>
-                                <div className='w-[3.5vw] rounded-full'>
-                                    <img
-                                        src={currentUser.profileImageUrl}
-                                        alt={currentUser.username} 
-                                        className='rounded-full'
-                                    />
-                                </div>
-                                <div>
-                                    <div className='flex flex-row gap-x-[2vw] items-center'>
-                                        <h3 className='hover:underline text-xl font-normal '>
-                                            {currentUser.username}
-                                        </h3>
-                                        <span>
-                                            {
-                                                UserInfo.uid === currentUser.uid ? 
-                                                <p className='text-orange-100 bg-orange-500 px-[1vw] py-[0.5vh] rounded-xl hover:opacity-80'>
-                                                    Creator
-                                                </p> 
-                                                :
-                                                <p className='text-orange-100 bg-orange-300 px-[1vw] py-[0.5vh] rounded-xl hover:opacity-80'>
-                                                    Supporter
-                                                </p>
-                                            }
-                                        </span>
-                                    </div>
-                                    <p>
-                                        {FormatDateSinceWhen(currentUser.birthDate)}
-                                    </p>
-                                </div>
-                            </>
-                        }
-                    </div>
                     <form
                         onSubmit={handleSubmit(OnCreatingReply)}
-                        className='flex flex-col gap-y-[1vh]'
+                        className='flex flex-row gap-y-[1vh] gap-x-[2vw]'
                     >
-                        <div>
+                        <div className='w-full'>
                             <Controller
                                 name="commentDetails"  // Fix the name to match your form input
                                 control={control}
@@ -126,12 +90,9 @@ export default function AddReplySection({ currentUser , parentComment }: Props) 
                                         className='w-full'
                                     />
                                 )}
-                            />
-                            {errors.commentDetails && (
-                                <p className="text-red-500">This field is required.</p>
-                            )}                             
+                            />                           
                         </div>
-                        <div className='flex w-full justify-end'>
+                        <div className='flex justify-end items-center'>
                             <button
                                 type="submit"
                                 className='flex flex-end  text-orange-50 bg-orange-400 px-[1.5vw] py-[1vh] rounded-lg hover:scale-[1.01] hover:bg-orange-500 duration-700 transition-all'
@@ -141,8 +102,11 @@ export default function AddReplySection({ currentUser , parentComment }: Props) 
                             </button>   
                         </div>
                     </form>
+                    {errors.commentDetails && (
+                        <p className="text-red-500">This field is required.</p>
+                    )}  
                 </div>
-
+                
         </div>
     </div>
     )
