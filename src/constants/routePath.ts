a67542ad8c3c780f2c00df5982bb1660fe3ab41e
ArKaRaShi <rnwz12345@gmail.com>
2024-09-16 21:11:07 +0700

    Add base url

--- src/constants/routePath.ts
+++ src/constants/routePath.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://localhost:3000";
 
 export const apiPath = {
     AUTH: {
