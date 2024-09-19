0f0f78d31f0dd4278209b4d3443708c9153e7569
ArKaRaShi <rnwz12345@gmail.com>
2024-09-19 23:07:46 +0700

    Remove comment

--- src/services/realtimeDbService/listenOnProjectChanges.ts
+++ src/services/realtimeDbService/listenOnProjectChanges.ts
@@ -3,14 +3,12 @@ import { ref, onValue, off } from "firebase/database";
 import { ProjectRealtimeDbModel } from "src/interfaces/realtimeDatabase/model";
 import { RealtimeDbRefPath } from "src/constants/realtimeDatabase/realtimeDbRefPath";
 
-// Function to listen for changes in Realtime Database
 export const listenOnProjectChanges = (
     projectId: string,
     callback: (data: ProjectRealtimeDbModel | null) => void
 ) => {
     const projectRef = ref(realtimeDbClient, `${RealtimeDbRefPath.PROJECT}/${projectId}`);
 
-    // Attach the listener
     const unsubscribe = onValue(projectRef, (snapshot) => {
         if (snapshot.exists()) {
             const projectData = snapshot.val();
@@ -20,6 +18,5 @@ export const listenOnProjectChanges = (
         }
     });
 
-    // Return a function to unsubscribe from the listener
     return () => off(projectRef);
 };
