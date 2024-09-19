c5186d30b24329a482f7cfd4f5c0474edaab2035
ArKaRaShi <55884696+ArKaRaShi@users.noreply.github.com>
2024-09-19 11:46:44 +0700

    Add session wrapper layer (#155)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,17 +3,20 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 import Layout from "src/components/global/Layout";
+import SessionWrapper from "src/components/global/SessionWrapper";
 
 export const metadata: Metadata = {
     title: "Shark WoW",
     description: "Funding for Startup and SMEs",
 };
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
             <body className="overflow-x-hidden">
-                <Layout>{children}</Layout>
+                <SessionWrapper initialData={null}>
+                    <Layout>{children}</Layout>
+                </SessionWrapper>
             </body>
         </html>
     );
