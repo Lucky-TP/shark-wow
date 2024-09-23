50b37c6b5ffd8a18d1d424c6558fb7d2190240fe
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-09-23 15:28:31 +0700

    Refactor using useCreatorSummaryProvider

--- src/app/(global)/(my-account)/creator/layout.tsx
+++ src/app/(global)/(my-account)/creator/layout.tsx
@@ -4,16 +4,14 @@ import React from 'react'
 
 import { UserInfo } from 'src/components/account/UserInfo'
 
-import { CreatorSummaryProvider } from 'src/context/creatorDashboard/useCreatorSummary'
-
 import { useUserData } from 'src/context/useUserData'
 
 
 export default function layout({ children }:{ children: React.ReactNode } ) {
     const { user, loading } = useUserData();
     return (
-        <CreatorSummaryProvider>
+        <section>
             <UserInfo user={user || undefined} />
-        </CreatorSummaryProvider>
+        </section>
     )
 }
--- src/app/(global)/(my-account)/creator/page.tsx
+++ src/app/(global)/(my-account)/creator/page.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
+import { CreatorSummaryProvider } from 'src/context/creatorDashboard/useCreatorSummary'
+
+
 type Props = {}
 
 export default function page({}: Props) {
   return (
-    <div>
-        page
-    
-    </div>
+    <CreatorSummaryProvider>
+        <section className="bg-[#E5D8CA] min-h-screen">
+            <div className="p-8">
+            {/* <Project /> */}
+            {/* <MyFundingProject/> */}
+            {/* <MyFavouriteProject /> */}
+            </div>
+        </section>
+    </CreatorSummaryProvider>
   )
 }
