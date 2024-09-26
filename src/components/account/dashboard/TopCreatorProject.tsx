dfeebff288e519912da428eee673fe811c690d8e
Toey <thanalast99@gmail.com>
2024-09-26 22:10:41 +0700

    handle name project

--- src/components/account/dashboard/TopCreatorProject.tsx
+++ src/components/account/dashboard/TopCreatorProject.tsx
@@ -17,7 +17,7 @@ export default function TopCreatorProject() {
                     <div className='flex items-center'>
                         <img src={project.previewImageUrl} alt={project.name} className='object-contain h-12 w-12 rounded-full mr-3'/>
                         <div>
-                            <h3 className='text-base font-semibold'>{project.name}</h3>
+                            <h3 className='text-base font-semibold w-[20vw] truncate whitespace-nowrap'>{project.name}</h3>
                             <p className='text-sm text-gray-600'>Funded {project.totalSupports} times</p>
                         </div>
                     </div>
