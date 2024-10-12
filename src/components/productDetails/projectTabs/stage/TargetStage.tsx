9842e16e6d01cfc347599f0852616a31e227b8a0
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-10-12 23:59:35 +0700

    refactor support button must sign in first

--- src/components/productDetails/projectTabs/stage/TargetStage.tsx
+++ src/components/productDetails/projectTabs/stage/TargetStage.tsx
@@ -165,6 +165,10 @@ export default function TargetStage({ stage }: Props) {
                 <div className="flex items-center justify-center w-full">
                     <button
                         onClick={async ()=>{
+                            if (!user) {
+                                router.push("/sign-in");
+                                return;
+                            }
                             const payload : CheckoutPayload = {
                                 projectId : ProjectInfo.projectId ?? "",
                                 fundingCost : Number((stage.goalFunding / (ProjectInfo?.totalQuantity || 1)).toFixed(2)),
