014bd9a47999d6337b12731e5378e87251a8bc9c
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-10-13 00:15:48 +0700

    refactor user must sign in before comment or reply

--- src/components/productDetails/projectTabs/CommentSection/AddReplySection.tsx
+++ src/components/productDetails/projectTabs/CommentSection/AddReplySection.tsx
@@ -12,6 +12,8 @@ import { CreateCommentPayload } from 'src/interfaces/payload/commentPayload';
 
 import { Input } from 'antd';
 import { FaReply } from "react-icons/fa";
+import { useUserData } from 'src/context/useUserData';
+import { useRouter } from 'next/navigation';
 
 
 type Props = {
@@ -33,7 +35,8 @@ const FormatDateSinceWhen = (date: string | undefined ):string  =>{
 } 
 
 export default function AddReplySection({ currentUser , parentComment }: Props) {
-    const { UserInfo , ProjectInfo , OnReFetchingData} = useProjectDetails();
+    // const { UserInfo , ProjectInfo , OnReFetchingData} = useProjectDetails();
+    const { user } = useUserData();
     const [disable,setDisable] = useState<boolean>(false)
     const { 
         reset,
@@ -48,9 +51,13 @@ export default function AddReplySection({ currentUser , parentComment }: Props) 
             }
         }
     )
+    const router = useRouter()
 
     const OnCreatingReply : SubmitHandler<IFormInput> = async (data) => {
         try{
+            if (!user ){
+                router.push('/sign-in')
+            }
             if (parentComment){ 
                 const payload : CreateCommentPayload = {
                     detail : data.commentDetails
@@ -69,45 +76,47 @@ export default function AddReplySection({ currentUser , parentComment }: Props) 
 
     return (
         <div className='flex flex-row w-full bg-orange-200 py-[1.5vh] pl-[2vw]'> 
-            <div className='flex flex-row w-full'>
-                <div className='w-[1vw] min-h-fit bg-orange-300 rounded-l-lg block'/>  
-                <div className='flex flex-col  bg-orange-100 p-4 border border-orange-300 rounded-r-xl w-full gap-y-[2vh]'>
-                    <form
-                        onSubmit={handleSubmit(OnCreatingReply)}
-                        className='flex flex-row gap-y-[1vh] gap-x-[2vw]'
-                    >
-                        <div className='w-full'>
-                            <Controller
-                                name="commentDetails"  // Fix the name to match your form input
-                                control={control}
-                                rules={{
-                                    required: 'This field is required.',
-                                }}
-                                render={({ field }) => (
-                                    <Input.TextArea
-                                        {...field}
-                                        placeholder={`Reply to this comment here...`}
-                                        className='w-full'
-                                    />
-                                )}
-                            />                           
-                        </div>
-                        <div className='flex justify-end items-center'>
-                            <button
-                                type="submit"
-                                className='flex flex-end  text-orange-50 bg-orange-400 px-[1.5vw] py-[1vh] rounded-lg hover:scale-[1.01] hover:bg-orange-500 duration-700 transition-all'
-                                disabled={disable}
-                            >
-                                <FaReply/>
-                            </button>   
-                        </div>
-                    </form>
-                    {errors.commentDetails && (
-                        <p className="text-red-500">This field is required.</p>
-                    )}  
+            {                
+                <div className='flex flex-row w-full'>
+                    <div className='w-[1vw] min-h-fit bg-orange-300 rounded-l-lg block'/>  
+                    <div className='flex flex-col  bg-orange-100 p-4 border border-orange-300 rounded-r-xl w-full gap-y-[2vh]'>
+                        <form
+                            onSubmit={handleSubmit(OnCreatingReply)}
+                            className='flex flex-row gap-y-[1vh] gap-x-[2vw]'
+                        >
+                            <div className='w-full'>
+                                <Controller
+                                    name="commentDetails"  // Fix the name to match your form input
+                                    control={control}
+                                    rules={{
+                                        required: 'This field is required.',
+                                    }}
+                                    render={({ field }) => (
+                                        <Input.TextArea
+                                            {...field}
+                                            placeholder={`Reply to this comment here...`}
+                                            className='w-full'
+                                        />
+                                    )}
+                                />                           
+                            </div>
+                            <div className='flex justify-end items-center'>
+                                <button
+                                    type="submit"
+                                    className='flex flex-end  text-orange-50 bg-orange-400 px-[1.5vw] py-[1vh] rounded-lg hover:scale-[1.01] hover:bg-orange-500 duration-700 transition-all'
+                                    disabled={disable}
+                                >
+                                    <FaReply/>
+                                </button>   
+                            </div>
+                        </form>
+                        {errors.commentDetails && (
+                            <p className="text-red-500">This field is required.</p>
+                        )}  
+                    </div>
+                    
                 </div>
-                
+            }
         </div>
-    </div>
     )
 }
