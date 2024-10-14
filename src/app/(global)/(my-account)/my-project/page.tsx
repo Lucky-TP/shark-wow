c94e97e3ea14337d82b17d3ec3686103f4575f06
ArKaRaShi <55884696+ArKaRaShi@users.noreply.github.com>
2024-10-14 19:47:20 +0700

    Feature/user api (#227)
    * chore: remove additional queries for projects and comments to reduce Firestore read costs
    
    * chore: remove unused files associated with data fields from getSelf service

--- src/app/(global)/(my-account)/my-project/page.tsx
+++ src/app/(global)/(my-account)/my-project/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Project from "src/components/account/project/MyProject";
 import MyFundingProject from "src/components/account/project/MyFundingProject";
 import MyFavouriteProject from "src/components/account/project/MyFavouriteProject";
 
@@ -9,7 +8,7 @@ export default function page({}: Props) {
     return (
         <section className="bg-[#E5D8CA] min-h-screen">
             <div className="p-8">
-                <Project />
+              
                 {/* <MyFundingProject/> */}
                 <MyFavouriteProject />
             </div>
