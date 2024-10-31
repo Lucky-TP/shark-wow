cb7431e4382b9de0eacbb6a557d5cbf534832dba
Toey <thanalast99@gmail.com>
2024-10-31 19:40:02 +0700

    add useinfo navbar

--- src/app/(global)/(my-account)/creator/page.tsx
+++ src/app/(global)/(my-account)/creator/page.tsx
@@ -5,14 +5,15 @@ import { CreatorSummaryProvider } from 'src/context/creatorDashboard/useCreatorS
 
 import CreatorDashboard from 'src/components/account/dashboard/CreatorDashboard'
 import { UserInfo } from 'src/components/account/UserInfo'
-
+import { useUserData } from 'src/context/useUserData'
 
 type Props = {}
 
 export default function page({}: Props) {
+  const { user, loading } = useUserData();
   return (
     <CreatorSummaryProvider>
-        <UserInfo />
+        <UserInfo user={user || undefined} />
         <CreatorDashboard/>
     </CreatorSummaryProvider>
   )
